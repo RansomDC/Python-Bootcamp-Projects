e6238a70135d70f11d9c2af0455d06dd0e7cc586
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-05-18 13:55:29 -0700

    Changed protected variables file in encapsulation so that they were all in one declared class instead of two.

--- Encapsulation assignment/protected variables.py
+++ Encapsulation assignment/protected variables.py
@@ -3,6 +3,7 @@ class Private():
     #this __init__ method will be run every time the Private() class is instantiated
     def __init__(self):
         self.__privateVar = "Stuff"
+        self._protectedVar = "cake"
 
     def editVar(self, value):
         self.__privateVar = value
@@ -15,16 +16,8 @@ priv.printVar()         #calling a method to print a private variable
 priv.editVar("junk")    #calling a method to edit a private variable
 priv.printVar()         #calling a method to print the (now changed) value of a private variable
 
-
 print("================")
 
-
-class Protected():
-    def __init__(self):
-        self._protectedVar = "cake"
-
-
-obj = Protected()           #creating an object from the Protected() class
-print(obj._protectedVar)    #printing the value of a protected variable
-obj._protectedVar = "pies"  #changing the value of a protected variable
-print(obj._protectedVar)    #printing the changed value of a protected variable
+print(priv._protectedVar)       #printing the value of a protected variable
+priv._protectedVar = "pies"     #changing the value of a protected variable
+print(priv._protectedVar)       #printing the changed value of a protected variable
