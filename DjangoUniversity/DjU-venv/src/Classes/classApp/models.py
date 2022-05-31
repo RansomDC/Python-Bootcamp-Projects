59d3cb5a60bb2e8ff751800e3b2d2b1ecfb39aae
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-05-31 14:45:55 -0700

    Adjusted field types in the Django Univeristy. Added IntegerField and FloatField to the course number and course duration respectively

--- DjangoUniversity/DjU-venv/src/Classes/classApp/models.py
+++ DjangoUniversity/DjU-venv/src/Classes/classApp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class djangoClasses(models.Model):
     title =  (models.CharField(max_length=60, default='', blank=True, null=False))
-    courseNumber = (models.CharField(max_length=20, default='', blank=True, null=False))
+    courseNumber = (models.IntegerField(max_length=20, blank=True, null=False))
     Instructor = (models.CharField(max_length=60, default='', blank=True, null=False))
-    Duration = (models.CharField(max_length=60, default='', blank=True, null=False))
+    Duration = (models.FloatField(max_length=60, blank=True, null=False))
 
     objects = models.Manager()
 
